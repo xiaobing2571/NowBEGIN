86720616feec04f9ea5a2ea0c2c9aab2c3df44fd
xiaobing2571 <325534051@qq.com>
2017-06-08 21:07:06 +0800

    hello
    Change-Id: Ibf751e2459299f495df75bea14ccd9fd6a4ebec0
    Signed-off-by: xiaobing2571 <325534051@qq.com>

--- src/GitHub.java
+++ src/GitHub.java
@@ -3,7 +3,7 @@ public class GitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("hello nowcode");
+			System.out.println("hello nowcode22");
 	}
 
 }
