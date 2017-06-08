acdb234323721588428556248e45b89f3353521a
xiaobing2571 <325534051@qq.com>
2017-06-08 21:46:10 +0800

    GIT1

--- src/GitHub.java
+++ src/GitHub.java
@@ -3,7 +3,7 @@ public class GitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("hello nowcode22");
+			System.out.println("hello nowcode2");
 	}
 
 }
