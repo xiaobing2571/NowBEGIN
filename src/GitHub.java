2f48403c82387bfac191f71992b40bb3a88fea3d
xiaobing2571 <325534051@qq.com>
2017-06-09 09:31:17 +0800

    hello

--- src/GitHub.java
+++ src/GitHub.java
@@ -6,6 +6,8 @@ public class GitHub {
 			System.out.println("hello nowcode2");
 			System.out.println("Aello nowcode2".codePointAt(0));
 			System.out.print("123");
+			System.out.print("123");
+			
 			
 	}
 
