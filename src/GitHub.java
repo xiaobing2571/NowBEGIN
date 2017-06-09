1121ef2c6decd5d90187267bf5d078c30bc8c7dd
xiaobing2571 <325534051@qq.com>
2017-06-09 09:23:00 +0800

    3

--- src/GitHub.java
+++ src/GitHub.java
@@ -4,6 +4,9 @@ public class GitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello nowcode2");
+			System.out.println("Aello nowcode2".codePointAt(0));
+			System.out.print("123");
+			
 	}
 
 }
